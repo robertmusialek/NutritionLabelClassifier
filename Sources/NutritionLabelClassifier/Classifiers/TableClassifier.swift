443cea470b96ebbfa22ff918a8f0bc4f95a0f496
Ahmed Khalaf <pxlshpr@gmail.com>
2022-06-12 02:21:36 +0500

    bump

--- Sources/NutritionLabelClassifier/Classifiers/TableClassifier.swift
+++ Sources/NutritionLabelClassifier/Classifiers/TableClassifier.swift
@@ -35,10 +35,41 @@ class TableClassifier {
     func getObservations() -> [Observation] {
 
         /// Identify column of labels
-        if let nutrientLabelTexts = getNutrientLabelTexts() {
-            let attributes = getUniqueAttributesFrom(nutrientLabelTexts)
+        if let attributeRecognizedTexts = getAttributeRecognizedTexts() {
+            let attributes = getUniqueAttributeTextsFrom(attributeRecognizedTexts)
             
-            let valueTexts = getValueLabelTexts()
+            let valueRecognizedTexts = getValueRecognizedTexts()
+            
+            let value1Texts: [ValueText]?
+            if let valueTexts = valueRecognizedTexts.0 {
+                value1Texts = valueTexts.compactMap {
+                    guard let value = Value(fromString: $0.string) else { return nil }
+                    return ValueText(value: value, text: $0)
+                }
+            } else {
+                value1Texts = nil
+            }
+            
+            let value2Texts: [ValueText]?
+            if let valueTexts = valueRecognizedTexts.1 {
+                value2Texts = valueTexts.compactMap {
+                    guard let value = Value(fromString: $0.string) else { return nil }
+                    return ValueText(value: value, text: $0)
+                }
+            } else {
+                value2Texts = nil
+            }
+            
+            //TODO: Next, we need to clean up columns by:
+            // 1. Detect that we have inline values with the attribute labels and then insert it accordingly into the column that is closest in terms of the y-value
+                // Also detect that we have merged two column values (such as energy with sliced cheese) in a value and use that to fill in both values accordingly
+            // 2. After this, use the largest column array (in terms of its count), to then space out and detect the empty or unread values in the smaller column by comparing the deltas between the y-values of elements (perhaps their centers?), and detecting the anomalies
+                // Test this on the Shredded Cheese Fibre value by making sure that gap is detected
+                // See if our ratio heuristic can fix this without the inline value before bringing it back in
+            // 3. Now that the values have the same number of rows, do another cleanup with the labels if the rows don't match
+                // First see if we have too many values, and if so, determine which labels are missing
+                    // If its energy and we have the energy units, fill it
+                // Now see if we have too many labels and if so, determine which value rows are missing, filling them with 0's (again using the y-value trick)
             print("Here we go")
         }
         
@@ -49,11 +80,11 @@ class TableClassifier {
     }
     
     //MARK: - Values
-    func getValueLabelTexts() -> ([RecognizedText]?, [RecognizedText]?) {
+    func getValueRecognizedTexts() -> ([RecognizedText]?, [RecognizedText]?) {
 
         var candidates: [[RecognizedText]] = [[]]
         
-        for recognizedTexts in arrayOfRecognizedTexts {
+        for recognizedTexts in [arrayOfRecognizedTexts.first ?? []] {
             for text in recognizedTexts {
                 guard let _ = Value(fromString: text.string) else {
                     continue
@@ -128,11 +159,20 @@ class TableClassifier {
     }
     
     //MARK: - Attributes
-    func getUniqueAttributesFrom(_ texts: [RecognizedText]) -> [Attribute]? {
-        texts.compactMap({ Attribute(fromString: $0.string) }).uniqued()
+    func getUniqueAttributeTextsFrom(_ texts: [RecognizedText]) -> [AttributeText]? {
+        var attributeTexts: [AttributeText] = []
+        for text in texts {
+            guard let attribute = Attribute(fromString: text.string),
+                  !attributeTexts.contains(where: { $0.attribute == attribute })
+            else {
+                continue
+            }
+            attributeTexts.append(AttributeText(attribute: attribute, text: text))
+        }
+        return attributeTexts
     }
     
-    func getNutrientLabelTexts() -> [RecognizedText]? {
+    func getAttributeRecognizedTexts() -> [RecognizedText]? {
         
         var candidates: [[RecognizedText]] = [[]]
         
