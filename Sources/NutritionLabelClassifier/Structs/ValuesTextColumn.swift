2c06ee2b6d5d10f69185b70f552c31e039c37f08
Ahmed Khalaf <pxlshpr@gmail.com>
2022-06-22 19:27:00 +0500

    bump

--- Sources/NutritionLabelClassifier/Structs/ValuesTextColumn.swift
+++ Sources/NutritionLabelClassifier/Structs/ValuesTextColumn.swift
@@ -86,9 +86,57 @@ extension ValuesTextColumn {
 }
 
 extension ValuesTextColumn {
+
+    //TODO: Improve this
+    /**
+     Returns a `CGRect` which represents a union of all the single-value `ValueText`'s of the column.
+
+     Any outliers that may span multple columns are ignored when calculating this union.
+     
+     Improve this by ignoring inline values that may be wider than the other single-value ValueText's. Do this by either ignoring those that also have an Attribute that can be extracted from the text or—better yet—write a function on ValueText that returns a boolean of whether the text contains any extraneous strings to the actual value strings and use this.
+     */
+    var columnRect: CGRect {
+        var unionRect: CGRect? = nil
+        for valuesText in valuesTexts {
+            
+            /// Skip values that don't have exactly one value
+            guard valuesText.values.count == 1 else {
+                continue
+            }
+            
+            guard let rect = unionRect else {
+                /// Set the first `ValuesText.rect` to be the `unionRect`
+                unionRect = valuesText.text.rect
+                continue
+            }
+            
+            /// Keep joining `unionRect` with any subsequent `ValuesText.rect`'s
+            unionRect = rect.union(valuesText.text.rect)
+        }
+        
+        /// Now return the union
+        return unionRect ?? .zero
+    }
     
-    /// Use `midX` of shortest text, checking if it lies within the shortest text of any column in each group
     func belongsTo(_ group: [ValuesTextColumn]) -> Bool {
+        
+        group.contains {
+            let rect = $0.columnRect
+            let yNormalizedRect = columnRect.rectWithYValues(of: rect)
+            return rect.intersection(yNormalizedRect) != .zero
+        }
+        
+//        guard let midX = valuesTexts.compactMap({ $0.text }).midXOfShortestText,
+//              let shortestText = group.shortestText
+//        else {
+//            return false
+//        }
+//
+//        return midX >= shortestText.rect.minX && midX <= shortestText.rect.maxX
+    }
+
+    func belongsTo_legacy(_ group: [ValuesTextColumn]) -> Bool {
+        /// Use `midX` of shortest text, checking if it lies within the shortest text of any column in each group
         //TODO-NEXT (2): Belongs to needs to be modified to recognize columns in spicy chips
         guard let midX = valuesTexts.compactMap({ $0.text }).midXOfShortestText,
               let shortestText = group.shortestText
